95c3a2abe7afb17ccca1e9bed921c0c3adbf6631
Zachary Nicolaou <zack.nicolaou@gmail.com>
2022-04-18 23:36:59 -0700

    defined matrices

--- viscid.py
+++ viscid.py
@@ -8,11 +8,174 @@ from scipy.special import iv
 
 #Return a numpy array corresponding to E^{klmn}_{k'l'm'n'} in Eqs. 31-39 in the notes. Return array has shape (3,3,2*Nt+1,2*Nt+1,2*Nx+1,2*Nx+1,2*Ny+1,2*Ny+1), with axes correponding to (k',k,l',l,m',m,n',n).
 def viscid_mat (omega, kx, ky):
-    #return array
-    E = np.zeros((3,3,2*args.Nt+1,2*args.Nt+1,2*args.Nx+1,2*args.Nx+1,2*args.Ny+1,2*args.Ny+1),dtype=np.complex128)
+    # return array
+    Ftilde,Gtilde=viscid_mat2 (omega, kx, ky)
+    E=Ftilde-args.ad*Gtilde
+    return E
+
+def viscid_mat2 (omega, kx, ky):
+    # return array
+    Ftilde = np.zeros((3, 3, 2*args.Nt+1, 2*args.Nt+1, 2*args.Nx+1, 2*args.Nx+1, 2*args.Ny+1, 2*args.Ny+1),dtype=np.complex128)
+    Gtilde = np.zeros((3, 3, 2*args.Nt+1, 2*args.Nt+1, 2*args.Nx+1, 2*args.Nx+1, 2*args.Ny+1, 2*args.Ny+1),dtype=np.complex128)
+
+    # mode indices. axes appear in the order (l',l,m',m,n',n)
+    lps = np.arange(-args.Nt, args.Nt + 1)[:, np.newaxis, np.newaxis, np.newaxis, np.newaxis, np.newaxis]
+    ls = np.arange(-args.Nt, args.Nt + 1)[np.newaxis, :, np.newaxis, np.newaxis, np.newaxis, np.newaxis]
+    mps = np.arange(-args.Nx, args.Nx + 1)[np.newaxis, np.newaxis, :, np.newaxis, np.newaxis, np.newaxis]
+    ms = np.arange(-args.Nx, args.Nx + 1)[np.newaxis, np.newaxis, np.newaxis, :, np.newaxis, np.newaxis]
+    nps = np.arange(-args.Ny, args.Ny + 1)[np.newaxis, np.newaxis, np.newaxis, np.newaxis, :, np.newaxis]
+    ns = np.arange(-args.Ny, args.Ny + 1)[np.newaxis, np.newaxis, np.newaxis, np.newaxis, np.newaxis, :]
+
+    kappax = kx + args.k1x*ms + args.k2x*ns
+    kappay = ky + args.k1y*ms + args.k2y*ns
+    kappa = (kappax**2+kappay**2)**0.5
+    Omega = 1j*(omega + 2*np.pi*args.freq*ls) + args.mu/args.rho*kappa**2
+
+    # Define also here the symmetric sums and differences C, Ctilde, S, Stilde above Eq. 31, and then assign the
+    # corresponding elements of E with array slicing.
+    C = np.exp(-kappa*args.h0) * iv(ms-mps, kappa*args.As*0.5) * iv(ns-nps, kappa*args.As*0.5) + \
+        np.exp(kappa*args.h0) * iv(ms-mps, -kappa*args.As*0.5) * iv(ns-nps, -kappa*args.As*0.5)
+
+    Ctilde = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * iv(ms-mps, (args.rho/args.mu*Omega)**0.5*args.As*0.5) * \
+             iv(ns-nps, (args.rho/args.mu*Omega)**0.5*args.As*0.5) + np.exp((args.rho/args.mu*Omega)**0.5*args.h0) * \
+             iv(ms-mps, -(args.rho/args.mu*Omega)**0.5*args.As*0.5) * iv(ns-nps, -(args.rho/args.mu*Omega)**0.5*args.As*0.5)
+
+    S = - np.exp(-kappa*args.h0) * iv(ms-mps, kappa*args.As*0.5) * iv(ns-nps, kappa*args.As*0.5) + \
+        np.exp(kappa*args.h0) * iv(ms-mps, -kappa*args.As*0.5) * iv(ns-nps, -kappa*args.As*0.5)
+
+    Stilde = - np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * iv(ms-mps, (args.rho/args.mu*Omega)**0.5*args.As*0.5) * \
+             iv(ns-nps, (args.rho/args.mu*Omega)**0.5*args.As*0.5) + np.exp((args.rho/args.mu*Omega)**0.5*args.h0) * \
+             iv(ms-mps, -(args.rho/args.mu*Omega)**0.5*args.As*0.5) * iv(ns-nps, -(args.rho/args.mu*Omega)**0.5*args.As*0.5)
+
+    Ftilde[0, 0][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        (Ctilde - (2*args.mu*kappax**2*C)/(args.rho*Omega+args.mu*kappa**2))
+
+    Ftilde[1, 0][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        2*args.mu*kappax*kappay*C/(args.rho*Omega+args.mu*kappa**2)
+
+    Ftilde[2, 0][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -1j*kappax*(2*args.mu*(args.rho/args.mu*Omega)**0.5 * \
+        (Ctilde-Stilde) + (args.rho*Omega+args.mu*kappa**2)*S/kappa + (args.g*args.rho**2+kappa**2*(args.rho*args.sigma - \
+        4*args.mu**2*(args.rho*Omega/args.mu)**0.5))*C/(args.rho*Omega+args.mu*kappa**2))/(2*args.rho)
+
+    Gtilde[2, 0][(np.arange(1, Ftilde.shape[2]), np.arange(Ftilde.shape[3]-1))] = -1j*args.rho*kappax*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,1:]
+
+    Gtilde[2, 0][(np.arange(Ftilde.shape[2]-1), np.arange(1, Ftilde.shape[3]))] = -1j*args.rho*kappax*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,:-1]
+
+    Ftilde[0, 1][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        2*args.mu*kappax*kappay*C/(args.rho*Omega+args.mu*kappa**2)
+
+    Ftilde[1, 1][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        (Ctilde - (2*args.mu*kappay**2*C)/(args.rho*Omega+args.mu*kappa**2))
+
+    Ftilde[2, 1][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -1j*kappay*(2*args.mu*(args.rho/args.mu*Omega)**0.5 * \
+        (Ctilde-Stilde) + (args.rho*Omega+args.mu*kappa**2)*S/kappa + (args.g*args.rho**2+kappa**2*(args.rho*args.sigma - \
+        4*args.mu**2*(args.rho*Omega/args.mu)**0.5))*C/(args.rho*Omega+args.mu*kappa**2))/(2*args.rho)
+
+    Gtilde[2, 1][(np.arange(1, Ftilde.shape[2]), np.arange(Ftilde.shape[3]-1))] = -1j*args.rho*kappay*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,1:]
+
+    Gtilde[2, 1][(np.arange(Ftilde.shape[2]-1), np.arange(1, Ftilde.shape[3]))] = -1j*args.rho*kappay*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,:-1]
+
+    Ftilde[0, 2][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = 1j*np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        kappax*(-2*args.mu*kappa*S/(args.rho*Omega+args.mu*kappa**2)+Stilde/(args.rho*Omega/args.mu)**0.5)
+
+    Ftilde[1, 2][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = 1j*np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        kappay*(-2*args.mu*kappa*S/(args.rho*Omega+args.mu*kappa**2)+Stilde/(args.rho*Omega/args.mu)**0.5)
+
+    Ftilde[2, 2][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = (-2*args.mu*kappa**2*(Ctilde-Stilde)+(args.rho*Omega + \
+        args.mu*kappa**2)*C+kappa*(args.g*args.rho**2+kappa**2*(args.rho*args.sigma-4*args.mu**2*(args.rho * \
+        Omega/args.mu)**0.5))*S/(args.rho*Omega+args.mu*kappa**2))/(2*args.rho)
+
+    Gtilde[2, 2][(np.arange(1, Ftilde.shape[2]), np.arange(Ftilde.shape[3]-1))] = args.rho*kappa*S/(4*(args.rho*Omega + \
+                                                                                                      args.mu*kappa**2))[:,1:]
+
+    Gtilde[2, 2][(np.arange(Ftilde.shape[2]-1), np.arange(1, Ftilde.shape[3]))] = args.rho*kappa*S/(4*(args.rho*Omega + \
+                                                                                                      args.mu*kappa**2))[:,:-1]
+
+    return Ftilde,Gtilde
+
+
+def viscid_flat_mat (omega, kx, ky):
+    # return array
+    Ftilde = np.zeros((3, 3, 2*args.Nt+1, 2*args.Nt+1),dtype=np.complex128)
+    Gtilde = np.zeros((3, 3, 2*args.Nt+1, 2*args.Nt+1),dtype=np.complex128)
+
+    # mode indices. axes appear in the order (l',l,m',m,n',n)
+    lps = np.arange(-args.Nt, args.Nt + 1)[:, np.newaxis]
+    ls = np.arange(-args.Nt, args.Nt + 1)[np.newaxis, :]
+
+    kappax = kx
+    kappay = ky
+    kappa = (kappax**2+kappay**2)**0.5
+    Omega = 1j*(omega + 2*np.pi*args.freq*ls) + args.mu/args.rho*kappa**2
+
+    # Define also here the symmetric sums and differences C, Ctilde, S, Stilde above Eq. 31, and then assign the
+    # corresponding elements of E with array slicing.
+    C = np.exp(-kappa*args.h0) + np.exp(kappa*args.h0)
+
+    Ctilde = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0)  + np.exp((args.rho/args.mu*Omega)**0.5*args.h0)
+
+    S = - np.exp(-kappa*args.h0)  + np.exp(kappa*args.h0)
+
+    Stilde = - np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) + np.exp((args.rho/args.mu*Omega)**0.5*args.h0)
+
+    Ftilde[0, 0][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        (Ctilde - (2*args.mu*kappax**2*C)/(args.rho*Omega+args.mu*kappa**2))
+
+    Ftilde[1, 0][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        2*args.mu*kappax*kappay*C/(args.rho*Omega+args.mu*kappa**2)
+
+    Ftilde[2, 0][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -1j*kappax*(2*args.mu*(args.rho/args.mu*Omega)**0.5 * \
+        (Ctilde-Stilde) + (args.rho*Omega+args.mu*kappa**2)*S/kappa + (args.g*args.rho**2+kappa**2*(args.rho*args.sigma - \
+        4*args.mu**2*(args.rho*Omega/args.mu)**0.5))*C/(args.rho*Omega+args.mu*kappa**2))/(2*args.rho)
+
+    Gtilde[2, 0][(np.arange(1, Ftilde.shape[2]), np.arange(Ftilde.shape[3]-1))] = -1j*args.rho*kappax*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,1:]
+
+    Gtilde[2, 0][(np.arange(Ftilde.shape[2]-1), np.arange(1, Ftilde.shape[3]))] = -1j*args.rho*kappax*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,:-1]
+
+    Ftilde[0, 1][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        2*args.mu*kappax*kappay*C/(args.rho*Omega+args.mu*kappa**2)
+
+    Ftilde[1, 1][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        (Ctilde - (2*args.mu*kappay**2*C)/(args.rho*Omega+args.mu*kappa**2))
+
+    Ftilde[2, 1][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = -1j*kappay*(2*args.mu*(args.rho/args.mu*Omega)**0.5 * \
+        (Ctilde-Stilde) + (args.rho*Omega+args.mu*kappa**2)*S/kappa + (args.g*args.rho**2+kappa**2*(args.rho*args.sigma - \
+        4*args.mu**2*(args.rho*Omega/args.mu)**0.5))*C/(args.rho*Omega+args.mu*kappa**2))/(2*args.rho)
+
+    Gtilde[2, 1][(np.arange(1, Ftilde.shape[2]), np.arange(Ftilde.shape[3]-1))] = -1j*args.rho*kappay*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,1:]
+
+    Gtilde[2, 1][(np.arange(Ftilde.shape[2]-1), np.arange(1, Ftilde.shape[3]))] = -1j*args.rho*kappay*C/(4*(args.rho*Omega +
+                                                                                                     args.mu*kappa**2))[:,:-1]
+
+    Ftilde[0, 2][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = 1j*np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        kappax*(-2*args.mu*kappa*S/(args.rho*Omega+args.mu*kappa**2)+Stilde/(args.rho*Omega/args.mu)**0.5)
+
+    Ftilde[1, 2][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = 1j*np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0) * \
+        kappay*(-2*args.mu*kappa*S/(args.rho*Omega+args.mu*kappa**2)+Stilde/(args.rho*Omega/args.mu)**0.5)
+
+    Ftilde[2, 2][(np.arange(Ftilde.shape[2]), np.arange(Ftilde.shape[3]))] = (-2*args.mu*kappa**2*(Ctilde-Stilde)+(args.rho*Omega + \
+        args.mu*kappa**2)*C+kappa*(args.g*args.rho**2+kappa**2*(args.rho*args.sigma-4*args.mu**2*(args.rho * \
+        Omega/args.mu)**0.5))*S/(args.rho*Omega+args.mu*kappa**2))/(2*args.rho)
+
+    Gtilde[2, 2][(np.arange(1, Ftilde.shape[2]), np.arange(Ftilde.shape[3]-1))] = args.rho*kappa*S/(4*(args.rho*Omega + \
+                                                                                                      args.mu*kappa**2))[:,1:]
+
+    Gtilde[2, 2][(np.arange(Ftilde.shape[2]-1), np.arange(1, Ftilde.shape[3]))] = args.rho*kappa*S/(4*(args.rho*Omega + \
+                                                                                                      args.mu*kappa**2))[:,:-1]
+
+    return Ftilde,Gtilde
+
+
+#Return numpy arrays corresponding to C^{mn}_{m'n'} and D^{mn}_{m'n'} in Eqs. 41-42 in the notes. Return array has shape (2*Nx+1,2*Nx+1,2*Ny+1,2*Ny+1), with axes correponding to (m',m,n',n).
+def inviscid_mat (kx, ky):
     #mode indices. axes appear in the order (l',l,m',m,n',n)
-    lps=np.arange(-args.Nt,args.Nt+1)[:,np.newaxis,np.newaxis,np.newaxis,np.newaxis,np.newaxis]
-    ls=np.arange(-args.Nt,args.Nt+1)[np.newaxis,:,np.newaxis,np.newaxis,np.newaxis,np.newaxis]
     mps=np.arange(-args.Nx,args.Nx+1)[np.newaxis,np.newaxis,:,np.newaxis,np.newaxis,np.newaxis]
     ms=np.arange(-args.Nx,args.Nx+1)[np.newaxis,np.newaxis,np.newaxis,:,np.newaxis,np.newaxis]
     nps=np.arange(-args.Ny,args.Ny+1)[np.newaxis,np.newaxis,np.newaxis,np.newaxis,:,np.newaxis]
@@ -21,23 +184,12 @@ def viscid_mat (omega, kx, ky):
     kappax = kx + args.k1x*ms + args.k2x*ns
     kappay = ky + args.k1y*ms + args.k2y*ns
     kappa = (kappax**2+kappay**2)**0.5
-    Omega = 1j*(omega + 2*np.pi*args.freq*ls) + args.mu/args.rho*kappa**2
 
-    #Define also here the symmetric sums and differences C,Ctile,S,Stilde above Eq. 31, and then assign the corresponding elements of E with array slicing.
-    Ctilde = np.exp(-(args.rho/args.mu*Omega)**0.5*args.h0)*iv(ms-mps,(args.rho/args.mu*Omega)**0.5*args.As/2)*iv(ns-nps,(args.rho/args.mu*Omega)**0.5*args.As/2) + np.exp((args.rho/args.mu*Omega)**0.5*args.h0)*iv(ms-mps,-(args.rho/args.mu*Omega)**0.5*args.As/2)*iv(ns-nps,-(args.rho/args.mu*Omega)**0.5*args.As/2)
     C = np.exp(-kappa*args.h0)*iv(ms-mps,kappa*args.As/2)*iv(ns-nps,kappa*args.As/2) + np.exp(kappa*args.h0)*iv(ms-mps,-kappa*args.As/2)*iv(ns-nps,-kappa*args.As/2)
+    S = -np.exp(-kappa*args.h0)*iv(ms-mps,kappa*args.As/2)*iv(ns-nps,kappa*args.As/2) + np.exp(kappa*args.h0)*iv(ms-mps,-kappa*args.As/2)*iv(ns-nps,-kappa*args.As/2)
 
-    E[0,0][(np.arange(E.shape[2]),np.arange(E.shape[3]))] = Ctilde-2*args.mu/args.rho*kappax**2*C/(args.mu*kappa**2+args.rho*Omega)
-
-
-    return E
-
-#Return numpy arrays corresponding to C^{mn}_{m'n'} and D^{mn}_{m'n'} in Eqs. 41-42 in the notes. Return array has shape (2*Nx+1,2*Nx+1,2*Ny+1,2*Ny+1), with axes correponding to (m',m,n',n).
-def inviscid_mat (omega, kx, ky):
-    #return arrays
-    F = np.array((2*args.Nx+1,2*args.Nx+1,2*args.Ny-1,2*args.Ny+1))
-    G = np.array((2*args.Nx+1,2*args.Nx+1,2*args.Ny-1,2*args.Ny+1))
-    #Define mode indices and other arrays as above
+    F=kappa*(args.g+args.sigma/args.rho*kappa**2)*(1-((ns-nps)*(args.k1x*kappax+args.k1y*kappay)+(ms-mps)*(args.k2x*kappax+args.k2y*kappay))/kappa**2)*S
+    G=-(1-((ns-nps)*(args.k1x*kappax+args.k1y*kappay)+(ms-mps)*(args.k2x*kappax+args.k2y*kappay))/kappa**2)*C
 
     return F,G
 
@@ -68,6 +220,10 @@ if __name__ == "__main__":
 
     #Find the inviscid undriven problem, and its eigenvalues and vectors
     F,G=inviscid_mat(args.kx,args.ky)
+    Fflattened=np.transpose(F[0,0],(0,2,1,3)).reshape(((2*args.Nx+1)*(2*args.Ny+1),(2*args.Nx+1)*(2*args.Ny+1)))
+    Gflattened=np.transpose(G[0,0],(0,2,1,3)).reshape(((2*args.Nx+1)*(2*args.Ny+1),(2*args.Nx+1)*(2*args.Ny+1)))
+    evals,evecs=eig(Fflattened,Gflattened)
+
     #save the matrices to with the filebase prefix
     F.tofile(args.filebase+'F.npy')
     G.tofile(args.filebase+'G.npy')
@@ -84,11 +240,11 @@ if __name__ == "__main__":
 else:
     #For benchmarking in jupyter
     class args:
-        Nx=3
-        Ny=3
-        Nt=3
+        Nx=5
+        Ny=5
+        Nt=5
         h0=0.1
-        As=0.01
+        As=0.05
         k1x=3**0.5/2*np.pi
         k1y=-0.5*np.pi
         k2x=0
